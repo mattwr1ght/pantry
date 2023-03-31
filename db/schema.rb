fc3d7630f06b259e17c39e27a516707b025fbbc8
Matt Wright <matthew.wright@appfolio.com>
2023-03-31 14:31:53 -0700

    Migration to add servings to recipes

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_27_175320) do
+ActiveRecord::Schema.define(version: 2023_03_31_212842) do
 
   create_table "food_items", force: :cascade do |t|
     t.string "item"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2022_04_27_175320) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "vegetarian", limit: 1
     t.boolean "vegan", limit: 1
+    t.integer "servings"
     t.index ["name", "cuisine", "vegan", "vegetarian"], name: "index_recipes_on_name_and_cuisine_and_vegan_and_vegetarian"
   end
 
