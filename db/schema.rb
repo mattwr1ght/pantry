9fb5c8b95b1f42ac87c98df6be77a0976dc075e1
Matt Wright <matthew.wright@appfolio.com>
2023-03-31 18:13:02 -0700

    Create table for Cuisine

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_31_212842) do
+ActiveRecord::Schema.define(version: 2023_03_31_234635) do
+
+  create_table "cuisines", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_cuisines_on_name", unique: true
+  end
 
   create_table "food_items", force: :cascade do |t|
     t.string "item"
