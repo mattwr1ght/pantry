496b592e0f452294f77b32d0281d18f688b7cad5
Matt Wright <matthew.wright@appfolio.com>
2023-04-24 21:52:05 -0700

    Add unit to inventories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_15_033512) do
+ActiveRecord::Schema.define(version: 2023_04_24_231058) do
 
   create_table "cuisines", force: :cascade do |t|
     t.string "name", null: false
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2023_04_15_033512) do
     t.decimal "quantity", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "unit"
     t.index ["food_item_id"], name: "index_inventories_on_food_item_id"
   end
 
